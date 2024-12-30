15d1523c4db0cc70a156f9633e455723ff62b580
Enoakpo Dennis <codetitan2206@gmail.com>
2024-12-30 13:48:09 +0100

    updated the function

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ func main() {
 	var conferenceName string = "Go Conference"
 	const conferenceticket int = 50
 	var remainTicket uint = 50
-	var bookings [50]string
+	//array
+	// var bookings [50]string
+	//slice
+	var bookings []string
 
 	fmt.Printf("Welcome to %v  booking application\n", conferenceName)
 	fmt.Printf("We have %v ticket avaliable and %v ticket left\n", conferenceticket, remainTicket)
@@ -34,10 +37,18 @@ func main() {
 
 	remainTicket = remainTicket - userTicket
 
-	bookings[0] = firstName + " " + lastName
-	fmt.Printf("The whole array: %v\n", bookings)
+	//bookings[0] = firstName + " " + lastName
+
+	bookings = append(bookings, firstName+" "+lastName)
+
+	/* fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array Type: %T\n", bookings) */
+
+	fmt.Printf("The whole slice: %v\n", bookings)
 	fmt.Printf("The first value: %v\n", bookings[0])
 	fmt.Printf("Array Type: %T\n", bookings)
+
 	fmt.Printf("Array Length: %v\n", len(bookings))
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receieve a confirmation email at %v.\n", firstName, lastName, userTicket, email)
 	fmt.Printf("%v tickets remaining for %v.\n", remainTicket, conferenceName)
